94a84f4fd9f47061b093e4925c343cefab4beb59
zhuzhuxia <zhuzhuyule@126.com>
2024-02-01 15:28:20 +0800

    init commit

---
+++ lib/src/main/animated_image.dart
@@ -0,0 +1,164 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:photo_wall/src/main/image_service.dart';
+import 'package:provider/provider.dart';
+
+
+class AnimationImage extends StatefulWidget {
+  const AnimationImage({Key? key, required this.url, this.onEnd})
+      : super(key: key);
+
+  final String url;
+  final Function? onEnd;
+
+  @override
+  _AnimationImageState createState() => _AnimationImageState();
+}
+
+class _AnimationImageState extends State<AnimationImage>
+    with TickerProviderStateMixin {
+  late Animation<double> leftAnimation;
+  late AnimationController leftController;
+
+  late Animation<double> scaleAnimation;
+  late AnimationController scaleController;
+
+  late Animation<double> rotateAnimation;
+  late AnimationController rotateController;
+
+  final GlobalKey _containerKey = GlobalKey();
+
+  getController({int seconds = 0, int milliseconds = 0}) {
+    return AnimationController(
+        duration: Duration(seconds: seconds, milliseconds: milliseconds),
+        vsync: this);
+  }
+
+  getAnimation(double begin, double end, AnimationController controller,
+      [bool repeat = true]) {
+    final tween = Tween(begin: begin, end: end);
+    late Animation animate;
+    if (repeat) {
+      final Animation<double> curve =
+          CurvedAnimation(parent: controller, curve: Curves.linear);
+      animate = tween.animate(curve);
+      animate = animate
+        ..addStatusListener((status) {
+          if (status == AnimationStatus.completed) {
+            controller.reverse();
+          } else if (status == AnimationStatus.dismissed) {
+            controller.forward();
+          }
+        });
+      controller.forward(from: tween.lerp(Random().nextDouble()));
+    } else {
+      final Animation<double> curve =
+          CurvedAnimation(parent: controller, curve: Curves.linear);
+      animate = tween.animate(curve);
+      controller.forward();
+    }
+
+    return animate;
+  }
+
+  @override
+  initState() {
+    super.initState();
+                      print('-----');
+    double screenWidth = window.physicalSize.width;
+
+    leftController = getController(seconds: 122);
+    leftAnimation =
+        getAnimation(screenWidth, -screenWidth, leftController, false);
+
+    // leftAnimation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     widget.onEnd?.call();
+    //   }
+    // });
+
+    scaleController = getController(seconds: 16);
+    scaleAnimation = getAnimation(1.0, 1.2, scaleController);
+
+    rotateController = getController(seconds: 15);
+    rotateAnimation = getAnimation(-0.1, 0.1, rotateController);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+          // var appState = context.watch<ImageService>();
+          // print(appState.list.length);
+    return AnimatedBuilder(
+        animation: rotateAnimation,
+        child: Image.network(widget.url),
+        builder: (context, child) {
+          return AnimatedBuilder(
+              animation: scaleAnimation,
+              child: child,
+              builder: (context, child) {
+                return Transform.scale(
+                  alignment: Alignment.center,
+                  scale: scaleAnimation.value,
+                  child: Transform.rotate(
+                    alignment: Alignment.bottomCenter,
+                    angle: rotateAnimation.value,
+                    child: LayoutBuilder(builder: (context, constraints) {
+
+                      getSizeOfContainer();
+                      return FractionallySizedBox(
+                        heightFactor: 0.75, // 高度为屏幕高度的80%
+                        child: Container(
+                            key: _containerKey,
+                            decoration: BoxDecoration(
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black54,
+                                  blurRadius: 10.0,
+                                  offset: Offset(0, 10.0),
+                                )
+                              ],
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 5, // 边框宽度
+                              ),
+                            ),
+                            transform: Matrix4.identity()
+                              ..translate(leftAnimation.value, 0, 0),
+                            child: child),
+                      );
+                    }),
+                  ),
+                );
+              });
+        });
+  }
+
+  void getSizeOfContainer() {
+    // 使用BuildContext获取RenderBox
+    if (_containerKey.currentContext != null) {
+      RenderBox renderBox =
+          _containerKey.currentContext?.findRenderObject() as RenderBox;
+
+      Size size = renderBox.size;
+      double width = size.width;
+
+      if (leftAnimation.value + width + 100 < 0) {
+        print("Element moved out of screen to the left!");
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          widget.onEnd?.call();
+        });
+      }
+    }
+  }
+
+  @override
+  dispose() {
+    leftController.dispose();
+    scaleController.dispose();
+    rotateController.dispose();
+    super.dispose();
+  }
+}
---
+++ lib/src/main/wall_view.dart
@@ -0,0 +1,87 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:photo_wall/src/main/image_service.dart';
+import 'package:provider/provider.dart';
+import 'animated_image.dart';
+
+class WallView extends StatefulWidget {
+  const WallView({super.key});
+
+  @override
+  _WallViewState createState() => _WallViewState();
+}
+
+class _WallViewState extends State<WallView> {
+  bool selected = false;
+  int count = 1;
+
+  List<String> list = [];
+
+  void addImage() {
+    setState(() {
+      count += 1;
+      list.add("https://picsum.photos/600/800?random$count");
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget buildAnimatedContainer(String url) {
+      return AnimationImage(
+          key: ValueKey(url),
+          url: url,
+          onEnd: () {
+            setState(() {
+              list.remove(url);
+            });
+            addImage();
+          });
+    }
+
+    return ChangeNotifierProvider(
+        create: (_) => ImageService(),
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Builder(builder: (context) {
+            // final imageService = Provider.of<ImageService>(context);
+            return Stack(
+              alignment: AlignmentDirectional.centerStart,
+              children: [
+                Image.network(
+                  'https://picsum.photos/600/800',
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: double.infinity,
+                ),
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                  child: Container(
+                    color: Colors.transparent,
+                  ),
+                ),
+                ...list
+                    .map((url) => buildAnimatedContainer(url))
+                    .toList(),
+                Positioned(
+                  bottom: 0,
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      addImage();
+            
+                    },
+                    child: const Icon(Icons.play_arrow),
+                  ),
+                )
+              ],
+            );
+          }),
+        ));
+  }
+}
